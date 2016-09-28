f8e5fd9a6f55d9189f795a09c05f90b89cddb61a
David Tucker <dbtucker@cal.berkeley.edu>
2016-09-28 14:56:55 -0700

    Fixed some comments.

--- instantiate-sandbox.sh
+++ instantiate-sandbox.sh
@@ -17,40 +17,13 @@
 
 #
 # Deploy the Confluent Platform components as a single-node install.
+# This is leveraged from the Azure template deployment script.
 #
 # Assumptions: all scripts included in the same directory.
 #
-# WARNING: The file upload process from the Azure templates CLEARS the
-#	execute bit on all files.   For that reason, we must to "sh <script>"
-#	when chaining them together here.
-#
 # USAGE :
 #	$0 
-#		--basename <basename> 
-#		--num-brokers <#brokers> 
-#		[ --num-zookeepers <#zk> ]		# must be 1 or 3
-#		[ --num-workers <#workers> ]
-#		[ --password <passwd for KafkaAdmin> ] 
 #
-# EXAMPLES :
-#	$0 --basename ktest --num-brokers 3
-#		A simple 3-node cluster ktest-broker-1, ktest-broker-2, ktest-broker-3
-#
-#	$0 --basename kcluster --num-brokers 5 --num-zookeepers 3 --num-workers 4
-#		A more complex cluster of 12 total nodes
-#			kcluster-broker-[1-5]
-#			kcluster-zk-[1-3]
-#			kcluster-worker-[1-4]
-#
-# NOTE:
-#	We take our queue regarding our role from the hostname.  We use
-#	the command line arguments to determine the config values to
-#	set in the properties files.
-#
-# TBD
-#	Probably don't need the <basename> property, since we can extract it
-#	from our own hostname ... but we'll keep it this way for now.
-#	
 
 
 THIS=`readlink -f $0`
